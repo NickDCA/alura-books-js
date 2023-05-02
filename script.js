42efef2f9e907f27bc89e2245895740f147a9840
nicolasdca <nicolasdavid193@gmail.com>
2023-05-02 15:55:25 -0300

    Aula 4

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error('Esse cep não existe');
-    } else {
-      console.log(r);
+async function buscaEndereco(cep) {
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error('CEP não existente!');
     }
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log('Processamento concluído! '));
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    console.log(erro);
+  }
+}
 
-console.log(consultaCEP);
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
